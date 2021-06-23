a854cf6a51367c09d73b108e7071a0df34b47791
Mohammed Mishat <69415903+M15H4T@users.noreply.github.com>
2021-06-23 13:22:54 +0600

    Fishing.py
    SocialFish - An Advanced Phishing Tool
    
     ▪️Installation:
    $ git clone https://github.com/UndeadSec/SocialFish.git
    $ cd SocialFish
    $ sudo pip install -r requirements.txt
    $ python3 SocialFish.py username your_password
    
    Then SocialFish will finish it's settings and we should be able to access the web interface by navigating to the URL http://0.0.0.0:5000/neptune in our browser.
    
     ▪️Usage:
    $ cd SocialFish
    $ python3 SocialFish.py youruser yourpassword
    
    Github: https://github.com/UndeadSec/SocialFish - An Advanced Phishing Tool
    
     ▪️Installation:
    $ git clone https://github.com/UndeadSec/SocialFish.git
    $ cd SocialFish
    $ sudo pip install -r requirements.txt
    $ python3 SocialFish.py username your_password
    
    Then SocialFish will finish it's settings and we should be able to access the web interface by navigating to the URL http://0.0.0.0: 5000/neptune in our browser.
    
     ▪️Usage:
    $ cd SocialFish
    $ python3 SocialFish.py youruser yourpassword
    
    Github: https://github. com/UndeadSec/SocialFish

---
+++ Fishing.py
@@ -0,0 +1,29 @@
+# Fishing-x1SocialFish - An Advanced Phishing Tool
+
+ ▪️Installation:
+$ git clone https://github.com/UndeadSec/SocialFish.git
+$ cd SocialFish
+$ sudo pip install -r requirements.txt
+$ python3 SocialFish.py username your_password
+
+Then SocialFish will finish it's settings and we should be able to access the web interface by navigating to the URL http://0.0.0.0:5000/neptune in our browser.
+
+ ▪️Usage:
+$ cd SocialFish
+$ python3 SocialFish.py youruser yourpassword
+
+Github: https://github.com/UndeadSec/SocialFish - An Advanced Phishing Tool
+
+ ▪️Installation:
+$ git clone https://github.com/UndeadSec/SocialFish.git
+$ cd SocialFish
+$ sudo pip install -r requirements.txt
+$ python3 SocialFish.py username your_password
+
+Then SocialFish will finish it's settings and we should be able to access the web interface by navigating to the URL http://0.0.0.0: 5000/neptune in our browser.
+
+ ▪️Usage:
+$ cd SocialFish
+$ python3 SocialFish.py youruser yourpassword
+
+Github: https://github. com/UndeadSec/SocialFish
